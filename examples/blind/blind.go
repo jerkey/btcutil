a174adde1c2c14781e75b480c08e97c2d113d9b3
jerkey <jerkey@halo.nu>
2014-11-28 02:36:11 -0500

    change example to blind a message, start adding comments

--- examples/blind/blind.go
+++ examples/blind/blind.go
@@ -1,34 +1,37 @@
 package main
 
-import "crypto/rand"
+//import "crypto/rand"
 import "fmt"
 import "github.com/mndrix/btcutil"
+//import "encoding/hex"
+import "math/big"
 
 func main() {
-	signer := new(btcutil.BlindSignerState)
-	requester := new(btcutil.BlindRequesterState)
+	signer := new(btcutil.BlindSignerState) // init w/Q (*pubkey) and later, d (secret), k (secret for request) but R (pubkey for request) is returned by BlindSession
+	requester := new(btcutil.BlindRequesterState) // contains F (pubkey), X0 (bigint) and Mhat (bigint)
 
 	// requester: message that needs to be blind signed
-	m, err := btcutil.RandFieldElement(rand.Reader)
-	maybePanic(err)
-	fmt.Printf("m = %x\n", m)
+	m:= new(big.Int).SetBytes([]byte("this is the message to blind-sign"))
+	//m, err := btcutil.RandFieldElement(rand.Reader)
+	//maybePanic(err)
+	fmt.Printf("m (message to be signed) = %x\n", m)
 
 	// requester: ask signer to start the protocol
-	Q, R := btcutil.BlindSession(signer)
-	fmt.Println("")
+	Q, R := btcutil.BlindSession(signer) // generates signer keypair once, and makes a pair for this request (stores signer pair and request.secret, returns both publics)
+	// fmt.Println(len)
 
 	// requester: blind message
-	mHat := btcutil.BlindMessage(requester, Q, R, m)
+	mHat := btcutil.BlindMessage(requester, Q, R, m) // 
 
 	// signer: create blind signature
 	sHat := btcutil.BlindSign(signer, R, mHat)
 
 	// requester extracts real signature
 	sig := btcutil.BlindExtract(requester, sHat)
-	sig.M = m
 	fmt.Printf("sig =\t%x\n\t%x\n", sig.S, sig.F.X)
 
 	// onlooker verifies signature
+	sig.M = m
 	if btcutil.BlindVerify(Q, sig) {
 		fmt.Printf("valid signature\n")
 	}
